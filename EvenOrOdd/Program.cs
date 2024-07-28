5127d17ff178404ed5ca2be38f3483e4968d4635
ahmed abnaser <ahmeducfjf@gmail.com>
2024-07-28 06:14:40 +0300

    Add project files.

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if ((num & 1) == 1)
+                Console.WriteLine($"this number {num} is Odd ");
+            else
+                Console.WriteLine($"this number {num} is Even ");
+        }
+    }
+}
